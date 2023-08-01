e0088cbf387eb8f724599d21bfda7e99f757e560
João Pedro Furlan <joaopedrofurlan1@hotmail.com>
2023-08-01 14:46:40 -0300

    Commit

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5, 8 , 9 ,2]
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O primeiro valor do vetor é ${num[0]}`)
+
+let pos = num.indexOf(8)
+
+console.log(`O valor 8 está na posição ${pos}`)
